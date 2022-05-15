2493810a171d456dd40641fde016a587d9168fa1
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-05-15 16:56:59 +0100

    Notes relation and extra field

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'age',
+        'excerpt',
         'email',
         'password',
     ];
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
 }
